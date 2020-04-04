576b63ee17c637c2d94883cf4710c0a4d3d8b39a
Raytch <rach.buchanan3@gmail.com>
2020-04-04 15:33:55 +1300

    Initial commit

---
+++ ChessBoardModel/Board.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+    public class Board
+    {
+        // size of board usually = 8x8
+        public int Size { get; set; }
+
+        // 2D array of type cell
+        public Cell[,] TheGrid { get; set; }
+
+        //constructor
+        public Board (int s, string cp)
+        {
+            // defines initial board size
+            Size = s;
+
+            // create new 2D array of type cell
+            TheGrid = new Cell[Size, Size];
+
+            // fill the 2D array with new cells, each with unique x & y coordinates
+            for (int i = 0; i < Size; i++)
+            {
+                for(int j = 0; j < Size; j++)
+                {
+                    TheGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            // clear all previous legal moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    TheGrid[i, j].LegalNextMove = false;
+                    TheGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+            // find all legal moves and mark the cells as legal
+            switch (chessPiece)
+            {
+                case "Knight":
+                    TheGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+
+                    TheGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+
+                    TheGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+
+                    TheGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+
+                    break;
+
+                case "King":
+                    break;
+
+                case "Rook":
+                    break;
+
+                case "Bishop":
+                    break;
+
+                case "Queen":
+                    break;
+
+                default:
+                    break;
+            }
+            TheGrid[currentCell.RowNumber, currentCell.ColumnNumber].CurrentlyOccupied = true;
+        }
+    }
+}
---
+++ ConsoleChess/Program.cs
@@ -0,0 +1,90 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChess
+{
+    class Program
+    {
+        static Board myBoard = new Board(8, "");
+
+        static void Main(string[] args)
+        {
+            // show the empty chess board
+            printBoard(myBoard);
+
+            // request coordinates from user where piece will be placed
+            Cell currentCell = setCurrentCell();
+            string chessPiece = setPieceType();
+            currentCell.CurrentlyOccupied = true;
+
+
+            // calculate all legal moves for that piece
+            myBoard.MarkNextLegalMoves(currentCell, chessPiece);
+
+            // print the chess board. Use an X for occupied square. Use a + for legal move. Use . for empty cell.
+            printBoard(myBoard);
+
+
+            // wait for user to press enter key before ending the program
+            Console.ReadLine();
+        }
+
+        private static string setPieceType()
+        {
+            Console.WriteLine("Which Chess piece would you like to place? Knight, King, Queen, Rook or Bishop");
+            string chessPiece = Console.ReadLine();
+
+            return chessPiece;
+        }
+
+        private static Cell setCurrentCell()
+        {
+            //get x & y coordinate from user. return a cell location.
+            Console.WriteLine("Enter the current Row number: ");
+            int currentRowNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the current Column number: ");
+            int currentColumnNum = int.Parse(Console.ReadLine());
+
+            return myBoard.TheGrid[currentRowNum, currentColumnNum];
+        }
+
+        private static void printBoard(Board myBoard)
+        {
+            // print the chess board Use an X for piece location. Use + for legal move. Use . for empty cell.
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                Console.WriteLine("+---+---+---+---+---+---+---+---+");
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    Console.Write("|");
+
+                    Cell c = myBoard.TheGrid[i, j];
+
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write(" x ");
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write(" + ");
+                    }
+                    else
+                    {
+                        Console.Write(" . ");
+                    }
+                }
+                Console.Write("|");
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("+---+---+---+---+---+---+---+---+");
+
+            Console.WriteLine("=====================================");
+        }
+    }
+}
